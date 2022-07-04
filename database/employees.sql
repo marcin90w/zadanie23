709802e632805f79d7c0764b523e7b49e5b39cd7
marcin90w <marcin90w@gmail.com>
2022-07-04 12:32:48 +0200

    solution with completed tasks

--- database/employees.sql
+++ database/employees.sql
@@ -16,6 +16,7 @@ VALUES
     ('Adam','Wojciechowicz',8000,'Cleaning','2000-01-01'),
     ('Robert','De Niro',3000,'Marketing','1961-01-01'),
     ('Steven','Seagal',9000,'Security','1965-01-01'),
+    ('test','test',1111,'test','2000-01-01'),
     ('Albert','Einstein',4500,'Sales','1879-03-14'),
     ('Marcin','Wajs',1000,'Accounter','1990-01-01');
 
@@ -28,55 +29,71 @@ SELECT * FROM employee WHERE (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FRO
 
 UPDATE employee SET salary = salary * 1.1 WHERE emp_department = 'Cleaning';
 
-SELECT * FROM employee ORDER BY birth_date DESC LIMIT 1;
+SELECT * FROM employee WHERE birth_date = (SELECT birth_date FROM employee ORDER BY birth_date DESC LIMIT 1);
 
 DROP TABLE employee;
+
 
 CREATE TABLE employee (
                           id INT PRIMARY KEY AUTO_INCREMENT,
                           first_name VARCHAR(30) NOT NULL,
-                          last_name VARCHAR(30) NOT NULL
+                          last_name VARCHAR(30) NOT NULL,
+                          department_id INT,
+                          address_id INT,
+                          FOREIGN KEY (department_id) REFERENCES department(id),
+                          FOREIGN KEY (address_id) REFERENCES address(id)
 );
 
 CREATE TABLE department (
                             id INT PRIMARY KEY AUTO_INCREMENT,
-                            employee_id INT UNIQUE NOT NULL,
                             emp_department VARCHAR(30) NOT NULL,
                             dep_description VARCHAR(100) NOT NULL,
-                            salary INT NOT NULL,
-                            FOREIGN KEY (employee_id) REFERENCES employee(id)
+                            salary INT NOT NULL
 );
 
 CREATE TABLE address (
                          id INT PRIMARY KEY AUTO_INCREMENT,
-                         employee_id INT UNIQUE NOT NULL,
                          street VARCHAR(30) NOT NULL,
                          localNumber VARCHAR(7) NOT NULL,
                          zip_code VARCHAR(8) NOT NULL,
-                         city VARCHAR(25) NOT NULL,
-                         FOREIGN KEY (employee_id) REFERENCES employee(id)
+                         city VARCHAR(25) NOT NULL
 );
 
 INSERT INTO department
-(employee_id, emp_department, dep_description, salary)
+(emp_department, dep_description, salary)
 VALUES
-    (1, 'Cleaning', 'W brudzie nie da się pracować', 8000),
-    (2, 'Management', 'bez Lidera to jak owce bez pasterza', 4000),
-    (4, 'IT', 'Zaktualizuje Ci Acrobat Readera', 5500),
-    (3, 'Accountant', 'Pozyskiwanie klientów', 2500);
+    ('Cleaning', 'W brudzie nie da się pracować', 8000),
+    ('Management', 'bez Lidera to jak owce bez pasterza', 4000),
+    ('IT', 'Zaktualizuje Ci Acrobat Readera', 5500),
+    ('Accountant', 'Pozyskiwanie klientów', 2500);
 
 INSERT INTO address
-(employee_id, street, localNumber, zip_code, city)
+(street, localNumber, zip_code, city)
 VALUES
-    (4, 'Warszawska', '4', '00-007', 'Warszawa'),
-    (3, 'Wrocławska', '120/22', '30-500', 'Wrocław'),
-    (2, 'Poznańska', '11b', '62-200', 'Poznań'),
-    (1, 'Gdańska', '33/2', '11-100', 'Gdańsk');
+    ('Warszawska', '4', '00-007', 'Warszawa'),
+    ('Wrocławska', '120/22', '30-500', 'Wrocław'),
+    ('Poznańska', '11b', '62-200', 'Poznań'),
+    ('Gdańska', '33/2', '11-100', 'Gdańsk');
 
 INSERT INTO employee
-(first_name, last_name)
+(first_name, last_name, department_id, address_id)
 VALUES
-    ('Robert', 'De Niro'),
-    ('Bogdan', 'Bogdalski'),
-    ('Wacław', 'Wacławski'),
-    ('Krzysztof', 'Krzysztofik');
+    ('Robert', 'De Niro', 1, 1),
+    ('Bogdan', 'Bogdalski', 2, 2),
+    ('Wacław', 'Wacławski', 1, 3),
+    ('Krzysztof', 'Krzysztofik', 3, 4),
+    ('Aleksander', 'Aleksandrowicz', 4, 1);
+
+SELECT
+    employee.first_name, employee.last_name, address.street, address.localNumber, address.zip_code, address.city,
+    department.emp_department, department.dep_description, department.salary
+FROM
+    employee
+JOIN	address
+ON		employee.address_id = address.id
+JOIN	department
+ON		employee.department_id = department.id;
+
+SELECT SUM(t.salary) AS totalSalary
+FROM (SELECT employee.id, department.salary
+      FROM (employee JOIN department ON employee.department_id = department.id)) AS t;
